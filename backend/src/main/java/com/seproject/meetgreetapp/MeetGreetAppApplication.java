e1d03e0c0d11f78987384351a995dac03e9d3a72
Nithin Pulivarthi <nithin@nithins-mbp.lan>
2020-09-18 16:16:44 -0400

    Added interests API

--- backend/src/main/java/com/seproject/meetgreetapp/MeetGreetAppApplication.java
+++ backend/src/main/java/com/seproject/meetgreetapp/MeetGreetAppApplication.java
@@ -1,13 +1,12 @@
 package com.seproject.meetgreetapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan("com.*")
 public class MeetGreetAppApplication {
 
 	public static void main(String[] args) {
