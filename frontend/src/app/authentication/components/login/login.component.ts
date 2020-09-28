2228ffc941d29143b3fdd6b66762b09abc64b650
sruthi paida <sruthi.paida7@gmail.com>
2020-09-28 16:08:27 -0500

    Commiting Login and Registration Modules

---
+++ frontend/src/app/authentication/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { FormControl, FormGroup, FormBuilder, Validator, ReactiveFormsModule, Validators } from "@angular/forms"
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../models';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  private loginComponent: LoginComponent;
+  public loginForm: FormGroup;
+  invalidLogin = {
+    status: false,
+    description: ''
+  };
+  constructor
+    (private fb: FormBuilder,
+      private router: Router,
+      private loginservice: AuthenticationService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    let loginRequest = new LoginRequest();
+    loginRequest = this.loginForm.value;
+  const res=this.loginservice.authenticate(loginRequest);
+    if(res.status ) {
+      this.router.navigate(['home']);
+    }
+    else {
+      this.invalidLogin = res;
+    }
+  }
+
+}
