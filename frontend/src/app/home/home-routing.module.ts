db4f5a479084524cd7ec6dddbc0f5be37fa6939d
pururajsinh <pururajsolanki111@gmail.com>
2020-10-19 16:56:08 -0500

    incomplete navigation and home

--- frontend/src/app/home/home-routing.module.ts
+++ frontend/src/app/home/home-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnnouncementComponent } from '../authentication/components/announcement/announcement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'announcement',
+    component: AnnouncementComponent
+    //canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule],
   exports: [RouterModule]
 })
 export class HomeRoutingModule { }
