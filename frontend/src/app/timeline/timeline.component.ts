d6b39ea1789279e34e55aaf4146de1faa6c0842d
Nishith Surya Singireddy <66642917+alpha1025@users.noreply.github.com>
2020-10-27 10:28:43 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	new file:   src/app/timeline/timeline.component.html 	new file:   src/app/timeline/timeline.component.scss 	new file:   src/app/timeline/timeline.component.spec.ts new file:   src/app/timeline/timeline.component.ts

---
+++ frontend/src/app/timeline/timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss']
+})
+export class TimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
