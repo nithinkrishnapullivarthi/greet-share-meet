94453082de188f24fadd5cd0e98ddc617baedfba
Nithin Pulivarthi <nithin@nithins-mbp.lan>
2020-10-30 01:12:54 -0400

    Implemented student details and student interests PUT API

--- backend/generated/open-api/src/main/java/com/seproject/meetgreetapp/Interest.java
+++ backend/generated/open-api/src/main/java/com/seproject/meetgreetapp/Interest.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.*;
 /**
  * Interest
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-10-21T14:20:59.893016-04:00[America/New_York]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-10-30T00:21:25.987437-04:00[America/New_York]")
 
 public class Interest   {
   @JsonProperty("id")
