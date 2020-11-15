a5cadc1ef531e90ffedce8f28dd7085d3dabe37d
SRK <srk@SRKs-Air.lan>
2020-11-15 12:21:44 -0600

    Nav bar removal on login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {Observable} from 'rxjs';
+import { AuthenticationService } from 'src/app/authentication/services/authentication.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  isLoggedIn$: Observable<boolean>;
+    constructor(private authService: AuthenticationService ) { }
+
+    ngOnInit(): void {
+      this.isLoggedIn$ = this.authService.isLoggedIn;
+    }
 }
