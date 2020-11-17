cfdb02c198c50a0edfe16ef17dcd2a26017ec416
Shashank <ponugotishashank@gmail.com>
2020-11-16 21:42:38 -0600

    integration and validation of view time slots

--- frontend/src/app/match-making/components/view-time-slots/view-time-slots.component.ts
+++ frontend/src/app/match-making/components/view-time-slots/view-time-slots.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HomeService } from '../../../home/services/home.service';
+
 
 @Component({
   selector: 'app-view-time-slots',
@@ -6,30 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-time-slots.component.scss']
 })
 export class ViewTimeSlotsComponent implements OnInit {
-  UserDetails: string;
 
-  details1 = [
-  {name:'shashank',
-  interest:'running',
-  sdate:'10/20/2020',
-  edate:'10/26/2020',
-  stime:'11:30 AM',
-  etime:'2:30 PM',
-  email:'ponugotishashank@gmail.com'
-  } 
-  ];;
-  details2 = [
-    {name:'prasad',
-    interest:'Physics',
-    sdate:'10/20/2020',
-    edate:'10/26/2020',
-    stime:'1:30 PM',
-    etime:'5:30 PM',
-    email:'prasad@gmail.com'
-    } 
-    ];;
-  ngOnInit(): void {
-    
-  }
+  constructor(private homeService: HomeService,
+    private fb: FormBuilder,
+    private router: Router, ) {
+
+    }
+
+  UserDetails: string;
+  errorMessage = null;
+  public usercards: any;
+   public userJson: any =[];
+
+    ngOnInit(): void {
+
+  let user = sessionStorage.getItem('user');
+  this.userJson = JSON.parse(user);
+  console.log('this is userjson',this.userJson.id);
+      this.homeService.getUserTimeSlot().subscribe(res => {
+         if(res){
+         this.usercards = res;
+         }
+         else{
+         this.errorMessage = "No matching slots found";
+         }
+      });
+}
 
 }
